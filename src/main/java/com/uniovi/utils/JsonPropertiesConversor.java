f51c938a1cd7ee84076c9297d18653e09f0b4dba
juanaza <juanaza@gordongrupo.com>
2018-03-24 20:04:54 +0100

    Now information in the dashboard is showed with two graphs
    A 3 dimensional pie chart represents the variety of states in which the incidents submitted are, a line graph shows the evolution of temperature of incidents submitted by sensors in a period of 1 hour each 5 minutes

---
+++ src/main/java/com/uniovi/utils/JsonPropertiesConversor.java
@@ -0,0 +1,27 @@
+package com.uniovi.utils;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+@Component
+public class JsonPropertiesConversor {
+	//http://www.techtamasha.com/convert-json-to-map-in-java/168
+	
+	public <K, V> String mapToJson(Map<String, Object> map) {
+		Gson gson = new Gson();
+		String json = gson.toJson(map);
+		return json;
+	}
+	
+	public <K, V> Map<K, V> jsonToMap(String json) {
+		Gson gson = new Gson();
+		Type type = new TypeToken<Map<K, V>>(){}.getType();
+		Map<K, V> map = gson.fromJson(json, type);		
+		return map;
+	}
+}
