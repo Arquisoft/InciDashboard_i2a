e5a3234aac19dd98a08122da8958ce482719a698
juanaza <juanaza@gordongrupo.com>
2018-04-29 18:40:38 +0200

    Prepare for merge

--- src/test/java/com/uniovi/InciDashboardI2aApplicationTests.java
+++ src/test/java/com/uniovi/InciDashboardI2aApplicationTests.java
@@ -3,6 +3,7 @@ package com.uniovi;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
 @RunWith(Suite.class)
 @SuiteClasses({
 	com.uniovi.tests_domain.IncidentTest.class,
