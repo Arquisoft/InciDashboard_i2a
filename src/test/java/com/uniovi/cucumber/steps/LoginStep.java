2de2ce32d8b2038145cec61561daf572c42f23ff
jesusatgar <jesus.atgar@gmail.com>
2018-05-08 21:40:50 +0200

    Try again

--- src/test/java/com/uniovi/cucumber/steps/LoginStep.java
+++ src/test/java/com/uniovi/cucumber/steps/LoginStep.java
@@ -1,7 +1,11 @@
 package com.uniovi.cucumber.steps;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.test.context.ContextConfiguration;
@@ -12,7 +16,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.uniovi.InciDashboardI2aApplication;
 
-import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -20,22 +23,22 @@ import cucumber.api.java.en.When;
 @ContextConfiguration(classes = InciDashboardI2aApplication.class, loader = SpringBootContextLoader.class)
 @WebAppConfiguration
 public class LoginStep {
-	
+
 	@Autowired
 	private WebApplicationContext context;
 
 	private MockMvc mvc;
-	
+
 	private WebDriver driver;
 	private String baseUrl;
 
 	@Before
 	public void setUp() throws Exception {
 		mvc = MockMvcBuilders.webAppContextSetup(context).build();
-	    driver = new HtmlUnitDriver();
-	    baseUrl = "http://localhost:8082";
-	    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	  }
+		driver = new HtmlUnitDriver();
+		baseUrl = "http://localhost:8082";
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
 
 	@When("^operator is in home page$")
 	public void Step2() {
@@ -46,15 +49,15 @@ public class LoginStep {
 	public void Step3() {
 
 	}
-	
+
 	@Then("^fills in correctly the email and password$")
 	public void Step4() {
 
 	}
-	
+
 	@Then("^operator is logged in$")
 	public void Step5() {
 
 	}
-	
+
 }
