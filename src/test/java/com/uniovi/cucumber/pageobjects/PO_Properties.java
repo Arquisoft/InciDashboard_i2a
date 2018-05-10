3fa990e25f78455d79ba889498695a159058676d
jesusatgar <jesus.atgar@gmail.com>
2018-05-10 19:16:16 +0200

    codacy number 134431243

--- src/test/java/com/uniovi/cucumber/pageobjects/PO_Properties.java
+++ src/test/java/com/uniovi/cucumber/pageobjects/PO_Properties.java
@@ -4,31 +4,29 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class PO_Properties {
-	public static int getSPANISH() {
-		return SPANISH;
-	}
 
-	public static int getENGLISH() {
-		return ENGLISH;
-	}
+	private static String Path;
+	private static int SPANISH = 0;
+	private static int ENGLISH = 1;	
+	private static Locale[] idioms = new Locale[] {new Locale("ES"), new Locale("EN")};
 
-	static private String Path;
-	static int SPANISH = 0;
-	static int ENGLISH = 1;	
-	static Locale[] idioms = new Locale[] {new Locale("ES"), new Locale("EN")};
-	//static Properties p = new Properties();
+	
 	public PO_Properties(String Path) //throws FileNotFoundException, IOException 
 	{
 		PO_Properties.Path = Path;
 	}
-	//
-	// locale is de index in idioms array.
-	//
     public String getString(String prop, int locale) {
 		
 		ResourceBundle bundle = ResourceBundle.getBundle(Path, idioms[locale]);
 		return bundle.getString(prop);
 	}
 
+    public static int getSPANISH() {
+		return SPANISH;
+	}
+
+	public static int getENGLISH() {
+		return ENGLISH;
+	}
 	
 }
