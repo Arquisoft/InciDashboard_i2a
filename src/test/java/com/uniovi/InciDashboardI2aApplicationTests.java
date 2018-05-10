f791ee0e59edb434f0205fd93a8e3f3fad686972
jesusatgar <jesus.atgar@gmail.com>
2018-05-10 18:23:51 +0200

    Conflicts solved

--- src/test/java/com/uniovi/InciDashboardI2aApplicationTests.java
+++ src/test/java/com/uniovi/InciDashboardI2aApplicationTests.java
@@ -7,13 +7,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	com.uniovi.tests_domain.IncidentTest.class,
-<<<<<<< HEAD
-	//com.uniovi.cucumber.CucumberTests.class
-=======
 	com.uniovi.tests_domain.OperatorTest.class,
 	com.uniovi.tests_domain.IncidentConversorTest.class,
 	com.uniovi.DatabaseTest.class
->>>>>>> master
 })
 public class InciDashboardI2aApplicationTests {
 
